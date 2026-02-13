358bc799dd621c28bb2cfaea65bd96875f04326f
Daniel <d.weimer777@gmail.com>
2026-02-13 08:47:11 +0100

    Input-word is equal to output-word, lowercase is in background

--- courses/coding-in-C/Lab_4/my_solutions/palindrome_S3.c
+++ courses/coding-in-C/Lab_4/my_solutions/palindrome_S3.c
@@ -15,23 +15,23 @@ int main(void){
     fgets(user_input, sizeof(user_input), stdin);
 
     int str_len = strlen(user_input);
-
     if (user_input[str_len - 1] != '\n') {
         clear_stdin();
     } else {
         user_input[str_len - 1] = '\0';
         str_len--;                
     }
-
-    for (int i = 0; user_input[i] != '\0'; i++) {
-        user_input[i] = tolower((unsigned char) user_input[i]);
-    }
+    
+    char user_input_lc[102];
+    for (int i = 0; user_input[i] != '\0'; i++)
+        user_input_lc[i] = tolower((unsigned char) user_input[i]);
+    user_input_lc[str_len] = '\0';
 
     char user_input_inverted[102];
-    for(int i=0; i<str_len; i++) user_input_inverted[i] = user_input[(str_len-1)-i];
+    for(int i=0; i<str_len; i++) user_input_inverted[i] = user_input_lc[(str_len-1)-i];
     user_input_inverted[str_len] = '\0';
 
-    if(strcmp(user_input, user_input_inverted) == 0){
+    if(strcmp(user_input_lc, user_input_inverted) == 0){
         printf("\nYour provided word \"%s\" is a palindrome.", user_input);
     }
     else{
