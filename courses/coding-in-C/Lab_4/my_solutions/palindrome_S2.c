3efa4118c90a1626e618e28cc91b400d3560f96d
Daniel <d.weimer777@gmail.com>
2026-02-13 08:13:30 +0100

    final palindrome Setective - Section 2 (optimized)

--- courses/coding-in-C/Lab_4/my_solutions/palindrome_S2.c
+++ courses/coding-in-C/Lab_4/my_solutions/palindrome_S2.c
@@ -20,7 +20,7 @@ int main(void){
 
     char user_input_inverted[102];
     for(int i=0; i<(str_len-1); i++) user_input_inverted[i] = user_input[(str_len-2)-i];
-    user_input_inverted[str_len] = '\0';
+    //user_input_inverted[str_len] = '\0';
 
     if(strcmp(user_input, user_input_inverted) == 0){
         printf("\nYour provided word \"%s\" is a palindrome.", user_input);
