005d154c3f6f5caaf292c7ef5b1599caeaf57a49
Daniel <d.weimer777@gmail.com>
2026-02-13 11:11:59 +0100

    Adding Function testing()

--- courses/coding-in-C/Lab_4/my_solutions/palindrome_S3.c
+++ courses/coding-in-C/Lab_4/my_solutions/palindrome_S3.c
@@ -7,10 +7,24 @@ void clear_stdin(void) {
     while ((c = getchar()) != '\n');
 }
 
+int testing(char input[], int lenght){
+    char input_lc[101];
+    for (int i = 0; input[i] != '\0'; i++)
+        input_lc[i] = tolower((unsigned char) input[i]);
+    input_lc[lenght] = '\0';
+
+    char input_inverted[101];
+    for(int i=0; i<lenght; i++) input_inverted[i] = input_lc[(lenght-1)-i];
+    input_inverted[lenght] = '\0';
+
+    return strcmp(input_lc, input_inverted);
+
+}
+
 
 int main(void){
 
-    char user_input[102];
+    char user_input[101];
     printf("Is your word a palindrome?\nType in your word (max. 100 characters):\n");
     fgets(user_input, sizeof(user_input), stdin);
 
@@ -22,16 +36,7 @@ int main(void){
         str_len--;                
     }
     
-    char user_input_lc[102];
-    for (int i = 0; user_input[i] != '\0'; i++)
-        user_input_lc[i] = tolower((unsigned char) user_input[i]);
-    user_input_lc[str_len] = '\0';
-
-    char user_input_inverted[102];
-    for(int i=0; i<str_len; i++) user_input_inverted[i] = user_input_lc[(str_len-1)-i];
-    user_input_inverted[str_len] = '\0';
-
-    if(strcmp(user_input_lc, user_input_inverted) == 0){
+    if(testing(user_input, str_len) == 0){
         printf("\nYour provided word \"%s\" is a palindrome.", user_input);
     }
     else{
