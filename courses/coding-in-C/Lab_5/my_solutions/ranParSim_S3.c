47b475d14dd7493b94b73851708782ff2735b5a1
Daniel <d.weimer777@gmail.com>
2026-02-14 11:14:51 +0100

    function move_randomly()

--- courses/coding-in-C/Lab_5/my_solutions/ranParSim_S3.c
+++ courses/coding-in-C/Lab_5/my_solutions/ranParSim_S3.c
@@ -19,6 +19,32 @@ void terminal(int time_counter, int size, int* array){
 }
 
 
+void move_randomly(int size, int* p, int* p_temp){
+    for(int i=0; i<10; i++) *(p_temp+i)=0;
+    
+    for(int i=0; i<size; i++){
+        if(*(p+i) == 1){
+            int zufall = rand()%2;
+            int target = i;
+
+            if((zufall == 1) && (i+1 < size)) 
+                target = i+1;
+            if((zufall == 0) && (i-1 >= 0)) 
+                target = i-1;
+                
+            if(*(p_temp + target) == 1){
+                *(p_temp + target) = 0;
+            }
+            else{
+                *(p_temp + target) = 1;
+            }
+        }
+    }
+
+    for(int i=0; i<size; i++) *(p+i) = *(p_temp+i);  
+}
+
+
 int main(){
 
     int array[] = {0,0,0,0,0,0,0,0,0,0};
@@ -26,44 +52,22 @@ int main(){
     array[4] = 1;
     array[6] = 1;
     array[8] = 1;
-    int size = sizeof(array) / sizeof(array[0]);
     int* p = array;
 
     int temp_array[10];
     int* p_temp = temp_array;
 
-    
-    srand(time(NULL));
+    int size = sizeof(array) / sizeof(array[0]);
     int time_counter = 0;
+    srand(time(NULL));
+    
     terminal(time_counter, size, array);
     
     while(has_one(array, size)){
         Sleep(1000);
-
-        for(int i=0; i<10; i++) *(p_temp+i)=0;
-    
-        for(int i=0; i<size; i++){
-            if(*(p+i) == 1){
-                int zufall = rand()%2;
-                int target = i;
-
-                if((zufall == 1) && (i+1 < size)) 
-                    target = i+1;
-                if((zufall == 0) && (i-1 >= 0)) 
-                    target = i-1;
-                
-                if(*(p_temp + target) == 1){
-                    *(p_temp + target) = 0;
-                }
-                else{
-                    *(p_temp + target) = 1;
-                }
-            }
-        }
-
-        for(int i=0; i<size; i++) array[i] = temp_array[i];
-
         time_counter++;
+
+        move_randomly(size, p, p_temp);
         terminal(time_counter, size, array);
     }
 }
