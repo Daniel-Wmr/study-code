7dc0ac1e5c11c5ddceff901a59648284187e0de8
Daniel <d.weimer777@gmail.com>
2026-02-25 21:37:54 +0100

    while-loop added

--- courses/coding-in-C/Lab_7/my_solutions/Lab_7_S1.c
+++ courses/coding-in-C/Lab_7/my_solutions/Lab_7_S1.c
@@ -5,33 +5,31 @@ int main() {
 
     int N;
 
-    printf("Type in an integer:\n");
+    while (1) {
+        printf("Type in an integer:\n");
 
-    // Eingabe prüfen
-    if (scanf("%d", &N) != 1) {
+        if (scanf("%d", &N) == 1) {
+            break;
+        }
+
         printf("Invalid input! Not an integer.\n");
 
-        // Eingabepuffer leeren
         int c;
         while ((c = getchar()) != '\n' && c != EOF) { }
-
-        return 1;   // Fehlercode
     }
 
-    // Speicher reservieren
     int* ptr = malloc(sizeof(int));
     if (ptr == NULL) {
         printf("Memory allocation failed!\n");
         return 1;
     }
 
-    // Wert speichern
     *ptr = N;
 
     printf("You entered %d and it is stored at memory address %p\n", *ptr, ptr);
 
-    // Speicher freigeben
     free(ptr);
 
     return 0;
 }
+
