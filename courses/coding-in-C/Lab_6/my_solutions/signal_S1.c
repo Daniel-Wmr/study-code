6658e76fdf764a4cd8155c54728021c91a0b22f1
Daniel <d.weimer777@gmail.com>
2026-02-25 09:33:39 +0100

    fgetc optimized

--- courses/coding-in-C/Lab_6/my_solutions/signal_S1.c
+++ courses/coding-in-C/Lab_6/my_solutions/signal_S1.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-
-int main(){
+int main() {
 
     FILE* pFile1 = fopen("file1.txt", "r");
-    if(pFile1 == NULL){
+    if (pFile1 == NULL) {
         printf("Datei konnte nicht geöffnet werden!\n");
         return -1;
     }
 
     int c;
-    while((c=fgetc(pFile1)) != EOF){
-        printf("Gelesen: %c\n", c);
+    while ((c = fgetc(pFile1)) != EOF) {
+        putchar(c);   // gibt das Zeichen exakt so aus, wie es ist
     }
 
     fclose(pFile1);
