5bb8889f2f8ce4514ff400179218ba5a2596bd5a
Daniel <d.weimer777@gmail.com>
2026-02-25 13:15:45 +0100

    fprintf added

--- courses/coding-in-C/Lab_6/my_solutions/signal_S1_write.c
+++ courses/coding-in-C/Lab_6/my_solutions/signal_S1_write.c
@@ -19,10 +19,21 @@
     // fputs – zeilenweises Schreiben
     fputs("Zeile 1: Hallo von fputs!\n", pFile2);
     fputs("Zeile 2: Noch eine Zeile.\n", pFile2);
-    fputs("Zeile 3: fputs schreibt Strings am Stück.\n", pFile2);
+    fputs("Zeile 3: fputs schreibt Strings am Stück.\n\n", pFile2);
+
+    printf("fputs wurde ausgeführt.\n\n");
+
+    // fprintf – formatiertes Schreiben
+    int number = 42;
+    const char* word = "Beispiel";
+
+    fprintf(pFile2, "Formatiertes Schreiben:\n");
+    fprintf(pFile2, "Zahl: %d\n", number);
+    fprintf(pFile2, "Wort: %s\n", word);
+    fprintf(pFile2, "Gemischt: Zahl=%d, Wort=%s\n", number, word);
 
     fclose(pFile2);
-    printf("fputs wurde ausgeführt.\n\n");
+    printf("fprintf wurde ausgeführt.\n\n");
 
  
     return 0;
