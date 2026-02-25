b7d35da5a9e878a36033d8781682f3c93dafadb1
Daniel <d.weimer777@gmail.com>
2026-02-25 09:52:53 +0100

    fgets added

--- courses/coding-in-C/Lab_6/my_solutions/signal_S1.c
+++ courses/coding-in-C/Lab_6/my_solutions/signal_S1.c
@@ -12,7 +12,20 @@ int main() {
     while ((c = fgetc(pFile1)) != EOF) {
         putchar(c);   // gibt das Zeichen exakt so aus, wie es ist
     }
-
     fclose(pFile1);
+    printf("\n\n");
+    
+    pFile1 = fopen("file1.txt", "r");
+    if (pFile1 == NULL) {
+        printf("Datei konnte nicht geöffnet werden!\n");
+        return -1;
+    }
+
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), pFile1) != NULL) { 
+        printf("%s", buffer);
+    } 
+    fclose(pFile1);
+    
     return 0;
 }
