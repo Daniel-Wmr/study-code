b89251ca0d886811753b41a59826aaf4b81d7697
Daniel <d.weimer777@gmail.com>
2026-02-25 22:00:29 +0100

    Adding calloc and realloc

--- courses/coding-in-C/Lab_7/my_solutions/Lab_7_S1.c
+++ courses/coding-in-C/Lab_7/my_solutions/Lab_7_S1.c
@@ -5,6 +5,7 @@ int main() {
 
     int N;
 
+    // Eingabeschleife
     while (1) {
         printf("Type in an integer:\n");
 
@@ -18,6 +19,7 @@ int main() {
         while ((c = getchar()) != '\n' && c != EOF) { }
     }
 
+    // Speicher für EINEN Integer
     int* ptr = malloc(sizeof(int));
     if (ptr == NULL) {
         printf("Memory allocation failed!\n");
@@ -26,10 +28,35 @@ int main() {
 
     *ptr = N;
 
-    printf("You entered %d and it is stored at memory address %p\n", *ptr, ptr);
+    printf("You entered %d and it is stored at memory address %p\n",
+           *ptr, (void*)ptr);
 
     free(ptr);
+    ptr = NULL;
+
+    // Speicher für N Integers
+    ptr = calloc(N, sizeof(int));
+    if (ptr == NULL) {
+        printf("Calloc failed!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        *(ptr + i) = i * i;
+    }
+
+    // Speicher auf 2*N Integers erweitern
+    int* temp = realloc(ptr, 2 * N * sizeof(int));
+    if (temp == NULL) {
+        printf("Realloc failed!\n");
+        free(ptr);   // alter Speicher ist noch gültig
+        return 1;
+    }
+    ptr = temp;
+
+    for (int i = N; i < 2 * N; i++) {
+        *(ptr + i) = i * i;
+    }
 
     return 0;
 }
-
