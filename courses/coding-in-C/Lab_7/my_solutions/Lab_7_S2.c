2c91e6d01f08acfd9890c56d40414133441a2599
Daniel <d.weimer777@gmail.com>
2026-02-25 23:36:43 +0100

    Creating array and inserting random number in index 3

--- courses/coding-in-C/Lab_7/my_solutions/Lab_7_S2.c
+++ courses/coding-in-C/Lab_7/my_solutions/Lab_7_S2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct Node {
     double value;
@@ -47,6 +48,14 @@ void print_list() {
     printf("NULL\n");
 }
 
+void print_array(int* array, int size){
+    for(int i = 0; i < size; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+
 int main() {
 
     for (int i = 0; i < 50; i++) {
@@ -54,6 +63,25 @@ int main() {
     }
 
     print_list();
+    printf("\n\n");
+
+    srand(time(NULL));
+    int array[51];
+
+    for(int i = 0; i < 50; i++){
+        int r = rand();
+        array[i] = r;
+    }
+    print_array(array, 50);
+    printf("\n\n");
+
+    int r_number = rand();
+    for(int i = 49; i > 2; i--){
+        array[i+1] = array[i];
+    }
+    array[3] = r_number;
+    print_array(array, 51);
+    printf("\n\n");
 
     return 0;
 }
