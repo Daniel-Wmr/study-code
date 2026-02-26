b6efcc807772dde7de055120a19ee1f572bcd069
Daniel <d.weimer777@gmail.com>
2026-02-26 16:51:33 +0100

    function add_song() added

--- courses/coding-in-C/Lab_7/my_solutions/Lab_7_S3.c
+++ courses/coding-in-C/Lab_7/my_solutions/Lab_7_S3.c
@@ -15,12 +15,37 @@ typedef struct Playlist{
 Playlist* init_playlist(){
     Playlist* p = malloc(sizeof(Playlist));
     if(!p) {
-        printf("Playlist Allocation failed!");
+        printf("Playlist Allocation failed!\n");
         return NULL;
     }
     p->head = NULL;
     return p;
 }
+
+void add_song(const char* name, const char* artist, Playlist* p){
+    Song* new_song = malloc(sizeof(Song));
+    if(!new_song) {
+        printf("Song allocation failed!\n");
+        return;
+    }
+
+    new_song->pName = strdup(name);
+    new_song->pArtist = strdup(artist);
+    new_song->pNext = NULL;
+
+    if(p->head == NULL){
+        p->head = new_song;
+        return;
+    }
+
+    Song* current = p->head;
+    while(current->pNext != NULL){
+        current = current->pNext;
+    }
+
+    current->pNext = new_song;
+}
+
 
 int main(){
     return 0;
