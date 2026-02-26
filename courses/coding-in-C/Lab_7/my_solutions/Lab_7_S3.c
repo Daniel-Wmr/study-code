4bec7fc59280f4c6a579cac62b6d67f16c4baac3
Daniel <d.weimer777@gmail.com>
2026-02-26 18:47:42 +0100

    Section 3 completed

--- courses/coding-in-C/Lab_7/my_solutions/Lab_7_S3.c
+++ courses/coding-in-C/Lab_7/my_solutions/Lab_7_S3.c
@@ -84,5 +84,20 @@ void delete_playlist(Playlist* p){
 }
 
 int main(){
+
+    Playlist* playlist = init_playlist();
+
+    add_song("Song1", "Artist1", playlist);
+    add_song("Song2", "Artist2", playlist);
+    add_song("Song3", "Artist3", playlist);
+
+    print_playlist(playlist);
+
+    delete_firstSong(playlist);
+    print_playlist(playlist);
+
+    delete_playlist(playlist);
+    printf("Success!\n");
+
     return 0;
 }
