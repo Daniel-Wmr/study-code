d605f116c511bd34a36648591aaa2bb84065cdc7
Daniel <d.weimer777@gmail.com>
2026-04-15 17:41:12 +0200

    helloWorld in C++ with header

--- courses/coding-in-C++/Lab_1/my_solution/helloWorld.cpp
+++ courses/coding-in-C++/Lab_1/my_solution/helloWorld.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
 
+#include "helloWorld.hpp"
+
+
 using namespace std;
+
+void printFromHeader()
+{
+    cout << "Hello World from .hpp" << endl;
+}
+
 
 int main()
 {
-    cout << "Hello Wolrd!" << endl;
+    cout << "Hello World!" << endl;
+    printFromHeader();
 
     return 0;
 }
